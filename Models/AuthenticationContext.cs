5aa53a9ca04204d403351bf96959bcab59a388cb
Stefan Ciuprina <stefan.ciuprina@gmail.com>
2021-04-10 16:43:04 +0300

    Initial create with login and register

---
+++ Models/AuthenticationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaboratoryActivityAPI.Models
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public DbSet<LaboratoryActivityAPI.Models.ApplicationUserModel> ApplicationUserModel { get; set; }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratoryActivityAPI.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Column()]
+        public int GroupId { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string Hobby { get; set; }
+
+        [Column(TypeName = "nvarchar(128)")]
+        public string Token { get; set; }
+
+        public string? Id { get; set; }
+        [ForeignKey("Id")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
